300ff6a521d291b82420a04ec55afb3dffec895c
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-06-22 14:01:08 +0100

    added markes - played with window features

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -3,17 +3,63 @@
 // # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 $(function(){
     function initialize(){
+      //for markers we need to define latling
+      // make a new object of it first!
+      var myLatlng = new google.maps.LatLng(51.506178, -0.088369);
       var mapOptions = {
         zoom: 12,
-        center: new google.maps.LatLng(51.506178, -0.088369),
+        center: myLatlng
       }
-      var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+      // we need to to put hte alting in a later postion so we can call it later 
+      // to add markers
+    //var image = 'doge.png' => doesn't work
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
+    var contentString = '<div>' + '<p>' +  'this is my content string!' + '</p>' + '</div>'
+    
+    var marker = new google.maps.Marker({
+      map:map,
+        // call the marker here
+      position: myLatlng,
+      // add some animation
+      animation: google.maps.Animation.DROP,
+      //icon:image,
+      title:"Hello World!" ,
+      }); 
+    // create our info window
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString
+      // we can specofy the content of this with say an ajax request from travel advisor!!!!
+    });
+      google.maps.event.addListener(marker, 'click', function(){
+        infowindow.open(map,marker);
+        toggleBounce
+      });
+      
+      function toggleBounce() {
+          if (marker.getAnimation() != null) {
+          marker.setAnimation(null);
+        } else {
+          marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+      }  
+      
     }
 
     initialize()
 
+    
+
   })
+
+// multiple makrer drop 
+// function drop() {
+//   for (var i =0; i < markerArray.length; i++) {
+//     setTimeout(function() {
+//       addMarkerMethod();
+//     }, i * 200);
+//   }
+// }
 
 
 
